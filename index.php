57af3e48387de63b8022f262ccb9d8cd6a645659
Mimi Rosnida Osman <mimiiosman93@gmail.com>
2018-03-03 15:55:18 +0800

    update home page

--- index.php
+++ index.php
@@ -14,16 +14,23 @@ if(!isset($_SESSION['valid'])) {
   <?php include_once('include/navbar.php'); ?>
   <!-- Navigation-->
   <div class="content-wrapper">
-    <div class="container-fluid">
+		<div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="#">Dashboard</a>
+          <a href="#">Halaman Utama</a>
         </li>
-        <li class="breadcrumb-item active">My Dashboard</li>
+        <li class="breadcrumb-item active">Utama</li>
       </ol>
-    </div>
-		<?php echo $_SESSION['valid']; ?>
+
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i> Maklumat System</div>
+          <div class="card-body">
+
+          </div>
+        </div>
+      </div>
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
     <!-- footer -->
