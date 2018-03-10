2262ab4ab27fef059b94954c75c84996fdc70138
Hafiz Azmi <hafizuddintgazmi@gmail.com>
2018-03-10 09:17:19 +0800

    crud diagnosis add user

---
+++ diagnosis/penyakit/edit_penyakit.php
@@ -0,0 +1,109 @@
+<?php session_start(); ?>
+
+<?php
+if(!isset($_SESSION['valid'])) {
+  header('Location: login.php');
+}
+?>
+
+<?php
+// including the database connection file
+include_once("../../include/connection.php");
+
+if(isset($_POST['update']))
+{
+  $id = $_POST['id'];
+
+  $name = $_POST['name'];
+  $detail = $_POST['detail'];
+
+  // checking empty fields
+  if(empty($name) || empty($detail)) {
+
+    if(empty($name)) {
+      echo "<font color='red'>Ruang Nama kosong.</font><br/>";
+    }
+
+    if(empty($detail)) {
+      echo "<font color='red'>Ruang Keterangan kosong.</font><br/>";
+    }
+  } else {
+    //updating the table
+    $result = mysqli_query($mysqli, "UPDATE `penyakit` SET `name`='$name',`detail`='$detail' WHERE id=$id")
+    or die("Could not execute the select query.");
+
+    //redirectig to the display page. In our case, it is view.php
+    header("Location: list_pengguna.php");
+  }
+}
+?>
+
+<?php
+//getting id from url
+$id = $_GET['id'];
+
+//selecting data associated with this particular id
+$result = mysqli_query($mysqli, "SELECT * FROM penyakit WHERE id=$id");
+
+while($res = mysqli_fetch_array($result))
+{
+  $name = $res['name'];
+  $detail = $res['detail'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once('../../include/header.php'); ?>
+
+  <body class="fixed-nav sticky-footer bg-dark" id="page-top">
+    <!-- Navigation-->
+    <?php include_once('../../include/navbar.php'); ?>
+    <!-- Navigation-->
+    <div class="content-wrapper">
+      <div class="container-fluid">
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Halaman Utama</a>
+          </li>
+          <li class="breadcrumb-item active">Tambah Penyakit</li>
+        </ol>
+
+        <div class="card mb-3">
+          <div class="card-header"><i class="fa fa-table"></i>  Tambah Penyakit</div>
+          <div class="card-body">
+            <form  name="add_penyakit" method="post" action="edit_penyakit.php">
+              <div class="form-group row">
+                <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="name" name="name" placeholder="Nama Penyakit" value="<?php echo $name;?>">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="detail" class="col-sm-2 col-form-label">Keterangan Penyakit</label>
+                <div class="col-sm-4">
+                  <textarea class="form-control" rows="5" id="detail" name="detail"><?php echo $detail;?></textarea>
+                </div>
+              </div>
+              <input type="submit" class="btn btn-primary" name="update" value="tambah">
+              <a href="list_penyakit.php" class="btn btn-danger">Kembali</a>
+            </form>
+          </div>
+          <!--<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>-->
+        </div>
+      </div>
+      <!-- /.container-fluid-->
+      <!-- /.content-wrapper-->
+
+
+      <!-- footer -->
+      <?php include_once('../../include/footer.php'); ?>
+      <!-- /.footer -->
+      <!-- script -->
+      <?php include_once('../../include/script.php'); ?>
+      <!-- /.script -->
+    </div>
+  </body>
+
+</html>
--- pengguna/add_pengguna.php
+++ pengguna/add_pengguna.php
@@ -5,31 +5,38 @@ if(!isset($_SESSION['valid'])) {
   header('Location: login.php');
 }
 ?>
-
 <?php
 // including the database connection file
 include_once("../include/connection.php");
 
-if(isset($_POST['update']))
+if(isset($_POST['add']))
 {
-  $id = $_POST['id'];
-
   $name = $_POST['name'];
   $username = $_POST['username'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
 
   // checking empty fields
-  if(empty($name) || empty($username)) {
+  if(empty($name) || empty($username) || empty($password) || empty($confirm_password)) {
 
     if(empty($name)) {
       echo "<font color='red'>Ruang nama kosong.</font><br/>";
     }
 
     if(empty($username)) {
-      echo "<font color='red'>Ruang email kosong.</font><br/>";
+      echo "<font color='red'>Ruang kata nama kosong.</font><br/>";
+    }
+
+    if(empty($password)) {
+      echo "<font color='red'>Ruang kata laluan kosong.</font><br/>";
+    }
+
+    if(empty($confirm_password)) {
+      echo "<font color='red'>Ruang sahkan kata laluan kosong.</font><br/>";
     }
   } else {
     //updating the table
-    $result = mysqli_query($mysqli, "UPDATE login SET name='$name', username='$username' WHERE id=$id")
+    $result = mysqli_query($mysqli, "INSERT INTO `login`(`name`, `email`, `username`, `password`) VALUES ('$name', '$username', '$username', md5('$password'))")
     or die("Could not execute the select query.");
 
     //redirectig to the display page. In our case, it is view.php
@@ -37,77 +44,64 @@ if(isset($_POST['update']))
   }
 }
 ?>
-<?php
-//getting id from url
-$id = $_GET['id'];
-
-//selecting data associated with this particular id
-$result = mysqli_query($mysqli, "SELECT * FROM login WHERE id=$id");
-
-while($res = mysqli_fetch_array($result))
-{
-  $name = $res['name'];
-  $username = $res['username'];
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include_once('../include/header.php'); ?>
 
-  <body class="fixed-nav sticky-footer bg-dark" id="page-top">
-    <!-- Navigation-->
-    <?php include_once('../include/navbar.php'); ?>
-    <div class="content-wrapper">
-      <div class="container-fluid">
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Halaman Utama</a>
-          </li>
-          <li class="breadcrumb-item active">Pentadbir</li>
-        </ol>
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!-- Navigation-->
+  <?php include_once('../include/navbar.php'); ?>
+  <div class="content-wrapper">
+    <div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Halaman Utama</a>
+        </li>
+        <li class="breadcrumb-item active">Pentadbir</li>
+      </ol>
 
-        <div class="card mb-3">
-          <div class="card-header">
-            <i class="fa fa-table"></i> Kemaskini Pengguna</div>
-            <div class="card-body">
-              <form name="form1" method="post" action="">
-                <div class="form-group">
-                  <div class="form-row">
-                    <div class="col-md-4">
-                      <label for="InputName">Nama Penuh</label>
-                      <input class="form-control" id="InputName" name="name" type="text" aria-describedby="nameHelp" placeholder="Masukkan Nama" required>
-                    </div>
-                  </div>
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i> Tambah Pengguna</div>
+          <div class="card-body">
+            <form  name="form1" method="post" action="add_pengguna.php">
+              <div class="form-group row">
+                <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                <div class="col-sm-6">
+                  <input type="text" class="form-control" id="nama" name="name" placeholder="Nama" value="">
                 </div>
-                <div class="form-group">
-                  <div class="form-row">
-                    <div class="col-md-4">
-                      <label for="InputEmail1">Emel</label>
-                      <input class="form-control" id="InputEmail1" name="username" type="email" aria-describedby="emailHelp" placeholder="Masukkan Emel" required>
-                    </div>
-                  </div>
+              </div>
+              <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Emel</label>
+                <div class="col-sm-4">
+                  <input type="email" class="form-control" id="username" name="username" placeholder="emel" value="">
                 </div>
-                <div class="form-group">
-                  <div class="form-row">
-                    <div class="col-md-4">
-                      <label for="InputPassword1">Katalaluan</label>
-                      <input class="form-control" id="InputPassword1" name="password" type="password" placeholder="KataLaluan" required>
-                    </div>
-                  </div>
+              </div>
+              <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Kata Laluan</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="password" name="password" placeholder="kata laluan" value="">
                 </div>
-                <input class="btn btn-success" type="submit" name="submit" value="Tambah">
-                <!-- <a class="btn btn-primary btn-block" href="login.php">Daftar</a> -->
-              </form>
-            </div>
+              </div>
+              <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Sahkan Kata Laluan</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="kata laluan" value="">
+                </div>
+              </div>
+              <input type="submit" class="btn btn-primary" name="add" value="tambah">
+              <a href="list_pengguna.php" class="btn btn-danger">Batal</a>
+            </form>
           </div>
         </div>
-        <!-- /.container-fluid-->
-        <!-- /.content-wrapper-->
-        <?php include_once('../include/footer.php'); ?>
-        <?php include_once('../include/script.php'); ?>
       </div>
-    </body>
+      <!-- /.container-fluid-->
+      <!-- /.content-wrapper-->
+      <?php include_once('../include/footer.php'); ?>
+      <?php include_once('../include/script.php'); ?>
+    </div>
+  </body>
 
-  </html>
+</html>
