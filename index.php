14157457df3e94f2237ca4a8a2b361b2d24e2f22
Mimi Rosnida Osman <mimiiosman93@gmail.com>
2018-05-18 17:58:35 +0800

    center the image

--- index.php
+++ index.php
@@ -23,22 +23,25 @@ if(!isset($_SESSION['valid'])) {
         <li class="breadcrumb-item active">Utama</li>
       </ol>
 
-      <div class="card col-sm-7">
-        <div class="card-header">
-          <i class="fa fa-table"></i> Maklumat System</div>
-					<img class="card-img-top" src="/static/img/sugar_glider3.png" alt="Card image cap">
-          <div class="card-body">
-				      <div class="rows">
-				        <h1  class="card-title">Sugar Glider</h1>
+
+        <!--<div class="card-header">-->
+          <!--<i class="fa fa-table"></i> Maklumat System</div>-->
+					<div class='a'>
+					<h1 align="center"> SELAMAT DATANG KE APLIKASI SISTEM PAKAR DIAGNOSIS PENYAKIT SUGAR GLIDER</h1>
+					<p text-align=center><img src="/static/img/sugar_glider3.png" width="550" height="350" class="center"></p>
+
+
+				      <!-- <div class="rows"> -->
+
 				        <!-- <div class="thumbnail">
 				          <img src="/static/img/sugar_glider3.png">
 				        </div> -->
-				        <p class="text-justify card-text"><em>Sugar Glider</em> atau nama saintifiknya <em>Pretaurus Breviceps</em> merupakan haiwan <em>marsupial</em> yangberasal dari Australia, dan sebahagian daripada Indonesia dan New Guinea. Haiwan ini bukan tergolong dalam spesies tupai terbang, dan diklasifikasikan sebagai haiwan eksotik di Amerika. <a data-toggle="collapse" data-target="#demo"><font style="color:red">Lagi..</font></a></p>
-				        <p class="text-justify collapse card-text" id="demo">Haiwan ini juga adalah haiwan <em>nocturnal</em> iaitu berjaga dan mencarimakanan pada waktu malam hari dan tidur sepanjang siang hari. Haiwan ini jika melahirkan anak, ia akan di jaga dan disusui di dalam kantung perut ibunya samasebagaimana kanggaru. Nama <em>Sugar Glider</em> datangnya daripada pemakanan mereka yang gemar makan buah-buahan manis (gula atau madu) dan <em>Glider</em> dari tingkahlaku mereka yang suka melompat dan menjunam menggunakan membran yang dinamakan <em>patagium</em>.</p>
-				      </div>
-          </div>
-        </div>
-      </div>
+
+				      <!-- </div> -->
+						</div>
+
+        <!--</div>-->
+
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
     <!-- footer -->
